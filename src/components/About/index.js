2549b2397c409d611796ea5cbb3c97b7e8c5d088
DiegoLHendrix <119289719+DiegoLHendrix@users.noreply.github.com>
2024-04-02 18:37:51 -0400

    About me simplification

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -50,17 +50,18 @@ const About = () => {
                             strArray={strSelf.split("")}
                             idx={15}
                         />
-
-                        <p className={'text-intro'}>
-                            <ul>
-                                I am a Computer Engineering Technology major with a passion for designing and
-                                integrating firmware, software, and embedded systems, as well as working with electrical
-                                equipment and PCB design. I am actively seeking an internship opportunity where I can
-                                leverage my skills in these areas and further challenge myself.
-                                Available May 2024 to December 2024.
-                            </ul>
-                        </p>
                     </h2>
+
+                    <p className={'text-about'}>
+                        <ul>
+                            I am a Computer Engineering Technology major with a passion for designing and
+                            integrating firmware, software, and embedded systems, as well as working with electrical
+                            equipment and PCB design. I am actively seeking an internship opportunity where I can
+                            leverage my skills in these areas and further challenge myself.
+                            Available May 2024 to December 2024.
+                        </ul>
+                    </p>
+
 
                     <h2 className={'title-education'}>
                         <AnimatedLetters
@@ -69,28 +70,29 @@ const About = () => {
                             strArray={strEducation.split("")}
                             idx={15}
                         />
-                        <p className={'education'}>
-                            <ul>
-                                <li>Bachelor of Science in Computer Engineering Technology.</li>
-                                <li>Expected Graduation May 2026.</li>
-                                <li>Dean's List Fall 2023.</li>
-                                <li>
-                                    <strong>Current Courses:</strong> Microcontroller Systems, Real-Time Operating
-                                    Systems, Modern Audio Production.
-                                </li>
-                                <li>
-                                    <strong>Completed Courses:</strong> Intro to Digital and Microcontroller Systems
-                                    Lecture and Lab, Digital Systems Design Lecture and Lab, Computational Problem
-                                    Solving 1 Lecture and Lab, DC/AC Circuits Lecture and Lab, Electronic Devices
-                                    Lecture and Lab, Computational Problem Solving 2, Fundamentals of Audio Engineering,
-                                    Networking Technologies.
-                                </li>
-                                <li>
-                                    <strong>Extracurriculars:</strong> Firmware Team in Electric Vehicle Team.
-                                </li>
-                            </ul>
-                        </p>
                     </h2>
+
+                    <p className={'text-about'}>
+                        <ul>
+                            <li>Bachelor of Science in Computer Engineering Technology.</li>
+                            <li>Expected Graduation May 2026.</li>
+                            <li>Dean's List Fall 2023.</li>
+                            <li>
+                                <strong>Current Courses:</strong> Microcontroller Systems, Real-Time Operating
+                                Systems, Modern Audio Production.
+                            </li>
+                            <li>
+                                <strong>Completed Courses:</strong> Intro to Digital and Microcontroller Systems
+                                Lecture and Lab, Digital Systems Design Lecture and Lab, Computational Problem
+                                Solving 1 Lecture and Lab, DC/AC Circuits Lecture and Lab, Electronic Devices
+                                Lecture and Lab, Computational Problem Solving 2, Fundamentals of Audio Engineering,
+                                Networking Technologies.
+                            </li>
+                            <li>
+                                <strong>Extracurriculars:</strong> Firmware Team in Electric Vehicle Team.
+                            </li>
+                        </ul>
+                    </p>
 
                     <h2 className={'title-skills'}>
                         <AnimatedLetters
@@ -99,33 +101,34 @@ const About = () => {
                             strArray={strSkills.split("")}
                             idx={15}
                         />
-                        <p className={'text-about'}>
-                            <ul>
-                                <li>
-                                    <strong>Languages:</strong> C, C++, Cmake, Python, VHDL.
-                                </li>
-                                <li>
-                                    <strong>Software:</strong> Altium Designer, Git, Github, FreeRTOS, CAN and CANopen,
-                                    SPI, UART, I2C, ADC, PCANView, STM32 Cube programmer/IDE/MX, CLion, Arduino IDE,
-                                    Code Composer Studio, Visual Studios Code/Community, Adobe Software, Microsoft
-                                    Office 365, Google docs/sheets/slides, Pro Tools 12, Audacity, Saleae Logic Pro.
-                                </li>
-                                <li>
-                                    <strong>Embedded:</strong> FreeRTOS, CANopen, SPI, UART, I2C, PCANView,
-                                    Saleae Logic Analyzer, STM32 Nucleo, Arduino Uno R3, TI MSP432,
-                                    Raspberry Pi 4, ESP32-S3.
-                                </li>
-                                <li>
-                                    <strong>Hardware:</strong> Saleae Logic Analyzer, Quartus II, ModelSim,
-                                    Multisim, Altera Cyclone II FPGA, Breadboards, Focusrite Scarlett 2i2.
-                                </li>
-                                <li>
-                                    <strong>Lab Equipment:</strong> Tektronix Oscilloscope, Digital Multimeter, Curve
-                                    Tracer, and Function Generator, Programmable DC Power Supplies, Soldering Iron.
-                                </li>
-                            </ul>
-                        </p>
                     </h2>
+
+                    <p className={'text-about'}>
+                        <ul>
+                            <li>
+                                <strong>Languages:</strong> C, C++, Cmake, Python, VHDL.
+                            </li>
+                            <li>
+                                <strong>Software:</strong> Altium Designer, Git, Github, FreeRTOS, CAN and CANopen,
+                                SPI, UART, I2C, ADC, PCANView, STM32 Cube programmer/IDE/MX, CLion, Arduino IDE,
+                                Code Composer Studio, Visual Studios Code/Community, Adobe Software, Microsoft
+                                Office 365, Google docs/sheets/slides, Pro Tools 12, Audacity, Saleae Logic Pro.
+                            </li>
+                            <li>
+                                <strong>Embedded:</strong> FreeRTOS, CANopen, SPI, UART, I2C, PCANView,
+                                Saleae Logic Analyzer, STM32 Nucleo, Arduino Uno R3, TI MSP432,
+                                Raspberry Pi 4, ESP32-S3.
+                            </li>
+                            <li>
+                                <strong>Hardware:</strong> Saleae Logic Analyzer, Quartus II, ModelSim,
+                                Multisim, Altera Cyclone II FPGA, Breadboards, Focusrite Scarlett 2i2.
+                            </li>
+                            <li>
+                                <strong>Lab Equipment:</strong> Tektronix Oscilloscope, Digital Multimeter, Curve
+                                Tracer, and Function Generator, Programmable DC Power Supplies, Soldering Iron.
+                            </li>
+                        </ul>
+                    </p>
 
                     <h2 className={'title-personal'}>
                         <AnimatedLetters
@@ -134,17 +137,17 @@ const About = () => {
                             strArray={strPersonal.split("")}
                             idx={15}
                         />
-                        <p className={'text-about'}>
-                            <ul>
-                                <li>Born in Massachusetts on November 2nd, 2002.</li>
-                                <li>From 2002-2010 lived in Santo Domingo, Dominican Republic.</li>
-                                <li>Moved to Massachusetts in 2010.</li>
-                                <li>Graduated from Malden Catholic High School in 2021.</li>
-                                <li>Started atteding Rochester Institute of Technology in 2021.</li>
-
-                            </ul>
-                        </p>
                     </h2>
+
+                    <p className={'text-about'}>
+                        <ul>
+                            <li>Born in Massachusetts on November 2nd, 2002.</li>
+                            <li>From 2002-2010 lived in Santo Domingo, Dominican Republic.</li>
+                            <li>Moved to Massachusetts in 2010.</li>
+                            <li>Graduated from Malden Catholic High School in 2021.</li>
+                            <li>Started atteding Rochester Institute of Technology in 2021.</li>
+                        </ul>
+                    </p>
 
                     <h2 className={'title-hobbies'}>
                         <AnimatedLetters
@@ -153,24 +156,26 @@ const About = () => {
                             strArray={strHobbies.split("")}
                             idx={15}
                         />
-                        <p className={'text-about'}>
-                            <ul>
-                                <li>Music:
-                                    <br/>Played guitar for 8 years
-                                    <br/>Take lessons at All that Music Academy in Stoneham, MA.
-                                </li>
-                                <li>Movies and TV
-                                    <br/>Favorite Move: Whiplash by Damien Chazelle.
-                                    <br/>Favorite Show: Avatar the Last Airbender.
-                                </li>
-                                <li>Programming and problem solving
-                                    <br/>
-                                </li>
-                            </ul>
-                        </p>
                     </h2>
 
+                    <p className={'text-about'}>
+                        <ul>
+                            <li>Music:
+                                <br/>Played guitar from 2017 to present.
+                                <br/>Take lessons at All that Music Academy in Stoneham, MA.
+                            </li>
+                            <li>Movies and TV
+                                <br/>Favorite Move: Whiplash by Damien Chazelle.
+                                <br/>Favorite Show: Avatar the Last Airbender.
+                            </li>
+                            <li>Programming and problem solving
+                                <br/>
+                            </li>
+                        </ul>
+                    </p>
+
                 </div>
+
                 <div className={'stage-cube-cont'}>
 
                     <div className={'cubespinner'}>
