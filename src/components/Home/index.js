defb0c5681cf31eee5a61eb5836b0c36887e247b
DiegoLHendrix <119289719+DiegoLHendrix@users.noreply.github.com>
2024-04-02 22:51:04 -0400

    Changed final project button label

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -39,9 +39,9 @@ const Home = () => {
                         idx={15}/>
                 </h1>
                 <h2>Embedded Software Engineer</h2>
-                <Link to={"/About"} className={'about-button'}>ABOUT ME</Link>
-                <Link to={"/Contact"} className={'contact-button'}>CONTACT ME</Link>
-                <Link to={"/FinalProject"} className={'final-button'}>FINAL PROJECT</Link>
+                {/*<Link to={"/About"} className={'about-button'}>ABOUT ME</Link>*/}
+                {/*<Link to={"/Contact"} className={'contact-button'}>CONTACT ME</Link>*/}
+                <Link to={"/FinalProject"} className={'final-button'}>MICROS FINAL PROJECT</Link>
             </div>
             <Logo />
         </div>
